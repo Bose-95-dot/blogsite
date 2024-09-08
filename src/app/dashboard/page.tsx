238adba601d29b3fb39a642a3536d905823cb37c
Sudipta Bose <149366183+SBosee@users.noreply.github.com>
2024-09-09 00:56:06 +0530

    UI changes for UI design part of blog

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Carasoul from "./carasoul"
+import BlogList from "./blogList"
+export default function Dashboard() {
+    return (
+        <>
+            <Carasoul />
+            <BlogList />
+        </>
+    )
+}
